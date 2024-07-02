9c448353422f8a5df8d61156d5d89317e39ed64a
Ansh Garhewal <me@anshdevs.in>
2024-07-02 22:52:01 +0530

    added scaler types

--- scaler_types/src/main.rs
+++ scaler_types/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hi, I am Crab's Claw!");
+
+    let decimal = 02_55;
+    let hex = 0xff;
+    let octal = 0o377;
+    let binary = 0b1111_1111;
+    let byte = b'G';
+
+    println!("{}",decimal);
+    println!("{}",hex);
+    println!("{}",octal);
+    println!("{}",binary);
+    println!("{}",byte);
+    
+    let defflt=56.5; // by defaults infers to f64 because it gives more precision
+    println!("{}",defflt);
+    
 }
