71563b9b71c36d0df0c800c09a1579afa8f2659e
Ansh Garhewal <me@anshdevs.in>
2024-10-19 17:44:45 +0530

    added binary heap in  collection

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,8 +1,14 @@
+
 use rand::seq::SliceRandom;
 use rand::thread_rng;
+use std::collections::BinaryHeap;
 
 fn main() {
+
+    // ================== Vectors =============================
+
     let mut nums:Vec<i32> = vec![];
+    
     nums.push(23);
     nums.push(1);
     nums.push(47);
@@ -13,6 +19,7 @@ fn main() {
     nums.push(79);
     nums.push(50);
     nums.push(60);
+
     let popper = nums.pop();
     print!("{:?}\n",popper);
     
@@ -31,4 +38,24 @@ fn main() {
 
     nums.shuffle(&mut thread_rng());
     println!("{:?}\n",nums);
+    // ================== Vectors =============================
+    
+
+
+    //  ================== Binary Heap =========================
+    let mut bheap= BinaryHeap::new();
+
+    bheap.push(1);
+    bheap.push(2);
+    bheap.push(3);
+    bheap.push(4);
+    bheap.push(5);
+    bheap.pop();
+
+    println!("{:?}\n",bheap);
+    println!("{:?}\n",bheap.peek());
+    
+
+    // ================== Binary Heap =========================
+
 }
